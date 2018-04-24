8d5ec8c779f22da5e9ae3ba5509bd24b0e1388bc
Timo <timokloks@hotmail.com>
2018-04-24 11:10:49 +0200

    standaard html/bootstrap file gemaakt

--- EenmaalAndermaal/detailpagina.php
+++ EenmaalAndermaal/detailpagina.php
@@ -1,3 +1,20 @@
+<!DOCTYPE html>
+<html lang="nl">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    </head>
+    <body>
+        <div class="container">
+            <h1>EenmaalAndermaal</h1>
+            <p>Feilose veilingsite</p>
+        </div>
+    </body>
+</html>
+
 <?php
 /**
  * Created by PhpStorm.
