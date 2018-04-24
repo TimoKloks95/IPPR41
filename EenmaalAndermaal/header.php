86bc98a1ce1dcb9e294b4e4c90f98217f7df80ac
Timo <timokloks@hotmail.com>
2018-04-24 14:21:49 +0200

    Header gemaakt

---
+++ EenmaalAndermaal/header.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Groep 41
+ * Date: 24-4-2018
+ * Time: 13:31
+ */
+    echo '
+    <header>
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-4">
+                    <img src="images/logo.png" width="150" height="100" alt="logo">
+                </div>
+                <div class="col-sm-4">
+                    <form>
+                        <input type="text" name="zoekterm"><br>
+                        <input type="submit" value="Search">
+                    </form>
+                </div>
+                <div class="col-sm-4">
+                    <form>
+                        <input type="submit" value="Login">
+                    </form>
+                </div>
+            </div>
+        </div>
+        <nav class="navbar navbar-default">
+            <div class="container-fluid">
+                <div class="navbar-header">
+                    <a class="navbar-brand" href="#">EenmaalAndermaal</a>
+                </div>
+                <ul class="nav navbar-nav">
+                    <li class="active"><a href="#">Home</a></li>
+                    <li><a href="#">Page 1</a></li>
+                    <li><a href="#">Page 2</a></li>
+                    <li><a href="#">Page 3</a></li>
+                </ul>
+            </div>
+        </nav>
+    </header>
+    ';
+?>
+
+
+
