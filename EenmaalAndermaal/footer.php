5673f44172cefaacff5074769c75b72e4b84b90a
Timo <timokloks@hotmail.com>
2018-04-25 11:49:47 +0200

    Header en Footer afgemaakt

--- EenmaalAndermaal/footer.php
+++ EenmaalAndermaal/footer.php
@@ -5,3 +5,47 @@
  * Date: 24-4-2018
  * Time: 13:31
  */
+    echo '
+        <br>
+        <div clas="d-flex flex-nowrap d-flex justify-content-around">
+            <nav class="d-flex flex-sm-nowrap d-flex justify-content-sm-around">
+            
+                <ul class="list-group">
+                     <li class="list-group-item">
+                        Links
+                    <ul class="list-group list-group-flush">
+                        <li class="list-group-item"><a href="#">#</a></li>
+                        <li class="list-group-item"><a href="#">#</a></li>
+                        <li class="list-group-item"><a href="#">#</a></li>
+                    </ul>
+                    </li>
+                </ul>
+                <ul class="list-group">
+                     <li class="list-group-item">
+                        Links
+                    <ul class="list-group list-group-flush">
+                        <li class="list-group-item"><a href="#">#</a></li>
+                        <li class="list-group-item"><a href="#">#</a></li>
+                        <li class="list-group-item"><a href="#">#</a></li>
+                    </ul>
+                    </li>
+                </ul>
+               <ul class="list-group">
+                     <li class="list-group-item">
+                        Links
+                    <ul class="list-group list-group-flush">
+                        <li class="list-group-item"><a href="#">#</a></li>
+                        <li class="list-group-item"><a href="#">#</a></li>
+                        <li class="list-group-item"><a href="#">#</a></li>
+                    </ul>
+                    </li>
+                </ul>
+            </nav>
+        </div><br>
+        <div class="d-flex justify-content-center">
+            <p class="d-flex justify-content-sm-center">
+                © Copyright door iConcepts
+            </p>
+        </div>
+    ';
+?>
--- EenmaalAndermaal/header.php
+++ EenmaalAndermaal/header.php
@@ -8,33 +8,31 @@
     echo '
     <header>
         <div class="container">
-            <div class="row">
-                <div class="col-sm-4">
-                    <img src="images/logo.png" width="150" height="100" alt="logo">
+            <div class="d-flex justify-content-around">
+                <div class="p-2">
+                    <img src="images/logo.png" class="img-fluid" width="160" height="70" alt="logo">
                 </div>
-                <div class="col-sm-4">
-                    <form>
-                        <input type="text" name="zoekterm"><br>
-                        <input type="submit" value="Search">
-                    </form>
+                <div class="p-2">
+                    <nav class="navbar">
+                        <form class="form-inline">
+                            <input class="form-control mr-sm-2" type="text" placeholder="Zoeken">
+                            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Zoeken</button>
+                        </form>
+                    </nav>
                 </div>
-                <div class="col-sm-4">
-                    <form>
-                        <input type="submit" value="Login">
-                    </form>
+                <div class="p-2">
+                     <button type="button" class="btn btn-default">Login</button>
+                     <button type="button" class="btn btn-default">Registreer</button>
                 </div>
             </div>
         </div>
-        <nav class="navbar navbar-default">
-            <div class="container-fluid">
-                <div class="navbar-header">
-                    <a class="navbar-brand" href="#">EenmaalAndermaal</a>
-                </div>
-                <ul class="nav navbar-nav">
-                    <li class="active"><a href="#">Home</a></li>
-                    <li><a href="#">Page 1</a></li>
-                    <li><a href="#">Page 2</a></li>
-                    <li><a href="#">Page 3</a></li>
+        <nav>
+            <div>
+                <ul class="nav justify-content-center">
+                    <li class="nav-item"><a class="nav-link active shadow p-3 mb-5 bg-light rounded" href="#">Home</a></li>
+                    <li class="nav-item"><a class="nav-link shadow p-3 mb-5 bg-light rounded" href="#">Page 1</a></li>
+                    <li class="nav-item"><a class="nav-link shadow p-3 mb-5 bg-light rounded" href="#">Page 2</a></li>
+                    <li class="nav-item"><a class="nav-link shadow p-3 mb-5 bg-light rounded" href="#">Page 3</a></li>
                 </ul>
             </div>
         </nav>
