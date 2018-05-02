9fa1de69a880c8de771455e9dee51794b082e630
Ilona <ilonavdwater@gmail.com>
2018-05-02 14:53:47 +0200

    Added app pages Fixed index.php

---
+++ EenmaalAndermaal/includes/database.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Ilona
+ * Date: 26/04/2018
+ * Time: 15:39
+ */
+
+?>
---
+++ EenmaalAndermaal/overzicht.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Ilona
+ * Date: 26/04/2018
+ * Time: 15:40
+ */
