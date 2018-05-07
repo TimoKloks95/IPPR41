6d3b303410e3f8f6989e9bd2589758130f514b81
Ilona <ilonavdwater@gmail.com>
2018-05-07 13:53:08 +0200

    Carousel aangepast

--- EenmaalAndermaal/detailpagina.php
+++ EenmaalAndermaal/detailpagina.php
@@ -6,109 +6,121 @@
  * Time: 15:40
  */
 ?>
-        <div class="container">
-            <div class="d-flex flex-sm-nowrap">
-                <div class="container-detail">
-                    <h2>Titel</h2>
-                    <div class="carousel">
-                        <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
-                            <ol class="carousel-indicators">
-                                <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-                                <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-                            </ol>
-                            <div class="carousel-inner" role="listbox">
-                                <div class="carousel-item active">
-                                    <img class="d-block w-100" src="images/klopt.jpg" data-src="holder.js/900x400?theme=social" alt="First slide">
-                                </div>
-                                <div class="carousel-item">
-                                    <img class="d-block w-100" src="images/ing_011.jpg" data-src="holder.js/900x400?theme=industrial" alt="Second slide">
-                                </div>
+    <div class="container">
+        <div class="d-flex flex-sm-nowrap">
+            <div class="container-detail">
+                <h2>Titel</h2>
+                <div class="carousel">
+                    <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+                        <ol class="carousel-indicators">
+                            <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+                            <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+                        </ol>
+                        <div class="carousel-inner" role="listbox">
+                            <div class="carousel-item active">
+                                <img class="d-block w-100" src="images/klopt.jpg"
+                                     data-src="holder.js/900x400?theme=social" alt="First slide">
                             </div>
-                            <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-                                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                                <span class="sr-only">Previous</span>
-                            </a>
-                            <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-                                <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                                <span class="sr-only">Next</span>
-                            </a>
+                            <div class="carousel-item">
+                                <img class="d-block w-100" src="images/ing_011.jpg"
+                                     data-src="holder.js/900x400?theme=industrial" alt="Second slide">
+                            </div>
                         </div>
+                        <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button"
+                           data-slide="prev">
+                            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                            <span class="sr-only">Previous</span>
+                        </a>
+                        <a class="carousel-control-next" href="#carouselExampleIndicators" role="button"
+                           data-slide="next">
+                            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                            <span class="sr-only">Next</span>
+                        </a>
                     </div>
-                    <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa.
-                        Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec,
-                        pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu.
-                        In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus.
-                        Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim.
-                        Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet.
-                        Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi. Nam eget dui.</p>
-                    <div id="informatieBlok">
-                        <ul class="nav nav-tabs">
-                            <li class="nav-item">
-                                <a class="nav-link active" href="#informatieBlok">Active</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="#informatieBlok">Link</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="#informatieBlok">Link</a>
-                            </li>
-                        </ul>
-                    </div>
-                    <h5>
-                        Titel
-                    </h5>
-                    <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa.
-                        Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec,
-                        pellentesque eu, pretium quis, sem.</p>
                 </div>
-                <div class="container-detailbieden">
-                    <div class="jumbotron">
-                        <h4>00:00</h4>
-                        <h6>Bieders</h6>
-                        <ul class="list-group">
-                            <li class="list-group-item">Richard - <strong>12.00</strong></li>
-                            <li class="list-group-item">Anna - <strong>10.50</strong></li>
-                        </ul>
-                        <div class="panel-group">
-                            <div class="panel panel-default">
-                                <div class="panel-heading">
-                                    <h6 class="panel-title">
-                                        <a data-toggle="collapse" href="#bieders">Oudere biedingen</a>
-                                    </h6>
-                                </div>
-                                <div id="bieders" class="panel-collapse collapse">
-                                    <ul class="list-group">
-                                        <li class="list-group-item">...</li>
-                                        <li class="list-group-item">...</li>
-                                        <li class="list-group-item">...</li>
-                                        <li class="list-group-item">...</li>
-                                        <li class="list-group-item">...</li>
-                                        <li class="list-group-item">...</li>
-                                        <li class="list-group-item">...</li>
-                                        <li class="list-group-item">...</li>
-                                        <li class="list-group-item">...</li>
-                                        <li class="list-group-item">...</li>
-                                        <li class="list-group-item">...</li>
-                                        <li class="list-group-item">...</li>
-                                        <li class="list-group-item">...</li>
-                                        <li class="list-group-item">...</li>
-                                        <li class="list-group-item">...</li>
-                                        <li class="list-group-item">...</li>
-                                        <li class="list-group-item">...</li>
-                                        <li class="list-group-item">...</li>
-                                        <li class="list-group-item">...</li>
-                                        <li class="list-group-item">...</li>
-                                        <li class="list-group-item">...</li>
-                                    </ul>
-                                </div>
+                <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean
+                    massa.
+                    Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam
+                    felis, ultricies nec,
+                    pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla
+                    vel, aliquet nec, vulputate eget, arcu.
+                    In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis
+                    pretium. Integer tincidunt. Cras dapibus.
+                    Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu,
+                    consequat vitae, eleifend ac, enim.
+                    Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus
+                    varius laoreet. Quisque rutrum. Aenean imperdiet.
+                    Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi. Nam eget dui.</p>
+                <div id="informatieBlok">
+                    <ul class="nav nav-tabs">
+                        <li class="nav-item">
+                            <a class="nav-link active" href="#informatieBlok">Active</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#informatieBlok">Link</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#informatieBlok">Link</a>
+                        </li>
+                    </ul>
+                </div>
+                <h5>
+                    Titel
+                </h5>
+                <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean
+                    massa.
+                    Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam
+                    felis, ultricies nec,
+                    pellentesque eu, pretium quis, sem.</p>
+            </div>
+            <div class="container-detailbieden">
+                <div class="jumbotron">
+                    <h4>00:00</h4>
+                    <h6>Bieders</h6>
+                    <ul class="list-group">
+                        <li class="list-group-item">Richard - <strong>12.00</strong></li>
+                        <li class="list-group-item">Anna - <strong>10.50</strong></li>
+                    </ul>
+                    <div class="panel-group">
+                        <div class="panel panel-default">
+                            <div class="panel-heading">
+                                <h6 class="panel-title">
+                                    <a data-toggle="collapse" href="#bieders">Oudere biedingen</a>
+                                </h6>
+                            </div>
+                            <div id="bieders" class="panel-collapse collapse">
+                                <ul class="list-group">
+                                    <li class="list-group-item">...</li>
+                                    <li class="list-group-item">...</li>
+                                    <li class="list-group-item">...</li>
+                                    <li class="list-group-item">...</li>
+                                    <li class="list-group-item">...</li>
+                                    <li class="list-group-item">...</li>
+                                    <li class="list-group-item">...</li>
+                                    <li class="list-group-item">...</li>
+                                    <li class="list-group-item">...</li>
+                                    <li class="list-group-item">...</li>
+                                    <li class="list-group-item">...</li>
+                                    <li class="list-group-item">...</li>
+                                    <li class="list-group-item">...</li>
+                                    <li class="list-group-item">...</li>
+                                    <li class="list-group-item">...</li>
+                                    <li class="list-group-item">...</li>
+                                    <li class="list-group-item">...</li>
+                                    <li class="list-group-item">...</li>
+                                    <li class="list-group-item">...</li>
+                                    <li class="list-group-item">...</li>
+                                    <li class="list-group-item">...</li>
+                                </ul>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
+    </div>
     </body>
-</html>
+    </html>
 
 <?php
 /**
--- EenmaalAndermaal/overzicht.php
+++ EenmaalAndermaal/overzicht.php
@@ -13,20 +13,20 @@
     <div class="row">
         <div class="col-sm shadow p-3 mb-5 bg-light rounded">
             <a href="?url=detailpagina">
-                One of three columns
                 <img src="images/Crow-Skull.png" class="col-sm-img" alt="Crow Skull">
+                One of three columns
             </a>
         </div>
         <div class="col-sm shadow p-3 mb-5 bg-light rounded">
             <a href="?url=detailpagina">
-                One of three columns
                 <img src="images/shoes.jpg" class="col-sm-img" alt="Shoes">
+                One of three columns
             </a>
         </div>
         <div class="col-sm shadow p-3 mb-5 bg-light rounded">
             <a href="?url=detailpagina">
-                One of three columns
                 <img src="images/wooden-closet.jpg" class="col-sm-img" alt="Wooden Closet">
+                One of three columns
             </a>
         </div>
     </div>
