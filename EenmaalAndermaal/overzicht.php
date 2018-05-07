d61b616b8e644a54880752a684819f19d31b286e
Ilona <ilonavdwater@gmail.com>
2018-05-07 13:40:58 +0200

    ...

--- EenmaalAndermaal/overzicht.php
+++ EenmaalAndermaal/overzicht.php
@@ -4,24 +4,30 @@
  * User: Ilona
  * Date: 26/04/2018
  * Time: 15:40
-<<<<<<< HEAD
+ * <<<<<<< HEAD
  */
 
 ?>
 
 <div class="container">
     <div class="row">
-        <div class="col-sm">
-            One of three columns
-            <img src="images/Crow-Skull.png" class="col-sm-img" alt="Crow Skull">
+        <div class="col-sm shadow p-3 mb-5 bg-light rounded">
+            <a href="?url=detailpagina">
+                One of three columns
+                <img src="images/Crow-Skull.png" class="col-sm-img" alt="Crow Skull">
+            </a>
         </div>
-        <div class="col-sm">
-            One of three columns
-            <img src="images/shoes.jpg" class="col-sm-img" alt="Shoes">
+        <div class="col-sm shadow p-3 mb-5 bg-light rounded">
+            <a href="?url=detailpagina">
+                One of three columns
+                <img src="images/shoes.jpg" class="col-sm-img" alt="Shoes">
+            </a>
         </div>
-        <div class="col-sm">
-            One of three columns
-            <img src="images/wooden-closet.jpg" class="col-sm-img" alt="Wooden Closet">
+        <div class="col-sm shadow p-3 mb-5 bg-light rounded">
+            <a href="?url=detailpagina">
+                One of three columns
+                <img src="images/wooden-closet.jpg" class="col-sm-img" alt="Wooden Closet">
+            </a>
         </div>
     </div>
 
--- logout.php
+++
@@ -1,7 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: Ilona
- * Date: 26/04/2018
- * Time: 15:41
- */
