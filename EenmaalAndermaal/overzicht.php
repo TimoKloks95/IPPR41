20224983ae678f7f3c0d578eec9d95a0c924e538
Ilona <ilonavdwater@gmail.com>
2018-05-09 12:29:56 +0200

    Overzichtpagina verbetert. je kunt nu op elk gedeelte van de kaart klinken om naar de detail pagina te gaan. En als je over de kaart hovert wordt er een background meegegeven

--- EenmaalAndermaal/overzicht.php
+++ EenmaalAndermaal/overzicht.php
@@ -10,84 +10,114 @@
 
 <div class="container bg-light">
     <main>
-        <div class="card">
-            <img class="card-img-top" src="images/Crow-Skull.png" alt="Card image cap">
-            <div class="card-body">
-                <h5 class="card-title carousel-caption">Kraaien schedel</h5>
-                <h6 class="card-subtitle mb-2 text-muted">[Prijs]</h6>
-                <h6 class="card-subtitle mb-2 text-muted">[Tijd]</h6>
-                <p class="card-text">De kraai is een in de symboliek vergelijkbaar met de raaf. Ook de kraai zou van
-                    oorsprong wit
-                    geweest zijn, in de Griekse sage wordt verteld dat de zonnegod Apollo zijn minnares, de
-                    koningsdochter Koronis, door een sneeuwwitte kraai....</p>
-                <a href="?url=detailpagina" class="btn btn-primary">Meer info</a>
+        <a href="?url=detailpagina">
+            <div class="card">
+                <img class="card-img-top" src="images/Crow-Skull.png" alt="Card image cap">
+                <div class="card-body">
+                    <h5 class="card-title carousel-caption">Kraaien schedel</h5>
+                    <h6 class="card-subtitle mb-2 text-muted">[Prijs]</h6>
+                    <h6 class="card-subtitle mb-2 text-muted">[Tijd]</h6>
+                    <p class="card-text">De kraai is een in de symboliek vergelijkbaar met de raaf. Ook de kraai zou van
+                        oorsprong wit
+                        geweest zijn, in de Griekse sage wordt verteld dat de zonnegod Apollo zijn minnares, de
+                        koningsdochter Koronis, door een sneeuwwitte kraai....
+                        <br>
+                        <br>
+                    </p>
+                    <a href="?url=detailpagina" class="btn btn-primary">Meer info</a>
+                </div>
             </div>
-        </div>
-        <div class="card">
-            <img class="card-img-top" src="images/make-up.jpg" alt="Card image cap">
-            <div class="card-body">
-                <h5 class="card-title carousel-caption">Make-up</h5>
-                <h6 class="card-subtitle mb-2 text-muted">[Prijs]</h6>
-                <h6 class="card-subtitle mb-2 text-muted">[Tijd]</h6>
-                <p class="card-text">De kraai is een in de symboliek vergelijkbaar met de raaf. Ook de kraai zou van
-                    oorsprong wit
-                    geweest zijn, in de Griekse sage wordt verteld dat de zonnegod Apollo zijn minnares, de
-                    koningsdochter Koronis, door een sneeuwwitte kraai....</p>
-                <a href="?url=detailpagina" class="btn btn-primary">Meer info</a>
+        </a>
+        <a href="?url=detailpagina">
+            <div class="card">
+                <img class="card-img-top" src="images/make-up.jpg" alt="Card image cap">
+                <div class="card-body">
+                    <h5 class="card-title carousel-caption">Make-up</h5>
+                    <h6 class="card-subtitle mb-2 text-muted">[Prijs]</h6>
+                    <h6 class="card-subtitle mb-2 text-muted">[Tijd]</h6>
+                    <p class="card-text">De kraai is een in de symboliek vergelijkbaar met de raaf. Ook de kraai zou van
+                        oorsprong wit
+                        geweest zijn, in de Griekse sage wordt verteld dat de zonnegod Apollo zijn minnares, de
+                        koningsdochter Koronis, door een sneeuwwitte kraai....
+                        <br>
+                        <br>
+                    </p>
+                    <a href="?url=detailpagina" class="btn btn-primary">Meer info</a>
+                </div>
             </div>
-        </div>
-        <div class="card">
-            <img class="card-img-top" src="images/shoes.jpg" alt="Card image cap">
-            <div class="card-body">
-                <h5 class="card-title carousel-caption">Schoenen</h5>
-                <h6 class="card-subtitle mb-2 text-muted">[Prijs]</h6>
-                <h6 class="card-subtitle mb-2 text-muted">[Tijd]</h6>
-                <p class="card-text">De kraai is een in de symboliek vergelijkbaar met de raaf. Ook de kraai zou van
-                    oorsprong wit
-                    geweest zijn, in de Griekse sage wordt verteld dat de zonnegod Apollo zijn minnares, de
-                    koningsdochter Koronis, door een sneeuwwitte kraai....</p>
-                <a href="?url=detailpagina" class="btn btn-primary">Meer info</a>
+        </a>
+        <a href="?url=detailpagina">
+            <div class="card">
+                <img class="card-img-top" src="images/shoes.jpg" alt="Card image cap">
+                <div class="card-body">
+                    <h5 class="card-title carousel-caption">Schoenen</h5>
+                    <h6 class="card-subtitle mb-2 text-muted">[Prijs]</h6>
+                    <h6 class="card-subtitle mb-2 text-muted">[Tijd]</h6>
+                    <p class="card-text">De kraai is een in de symboliek vergelijkbaar met de raaf. Ook de kraai zou van
+                        oorsprong wit
+                        geweest zijn, in de Griekse sage wordt verteld dat de zonnegod Apollo zijn minnares, de
+                        koningsdochter Koronis, door een sneeuwwitte kraai....
+                        <br>
+                        <br>
+                    </p>
+                    <a href="?url=detailpagina" class="btn btn-primary">Meer info</a>
+                </div>
             </div>
-        </div>
-        <div class="card">
-            <img class="card-img-top" src="images/wooden-closet.jpg" alt="Card image cap">
-            <div class="card-body">
-                <h5 class="card-title carousel-caption">Houten kast</h5>
-                <h6 class="card-subtitle mb-2 text-muted">[Prijs]</h6>
-                <h6 class="card-subtitle mb-2 text-muted">[Tijd]</h6>
-                <p class="card-text">De kraai is een in de symboliek vergelijkbaar met de raaf. Ook de kraai zou van
-                    oorsprong wit
-                    geweest zijn, in de Griekse sage wordt verteld dat de zonnegod Apollo zijn minnares, de
-                    koningsdochter Koronis, door een sneeuwwitte kraai....</p>
-                <a href="?url=detailpagina" class="btn btn-primary">Meer info</a>
+        </a>
+        <a href="?url=detailpagina">
+            <div class="card">
+                <img class="card-img-top" src="images/wooden-closet.jpg" alt="Card image cap">
+                <div class="card-body">
+                    <h5 class="card-title carousel-caption">Houten kast</h5>
+                    <h6 class="card-subtitle mb-2 text-muted">[Prijs]</h6>
+                    <h6 class="card-subtitle mb-2 text-muted">[Tijd]</h6>
+                    <p class="card-text">De kraai is een in de symboliek vergelijkbaar met de raaf. Ook de kraai zou van
+                        oorsprong wit
+                        geweest zijn, in de Griekse sage wordt verteld dat de zonnegod Apollo zijn minnares, de
+                        koningsdochter Koronis, door een sneeuwwitte kraai....
+                        <br>
+                        <br>
+                    </p>
+                    <a href="?url=detailpagina" class="btn btn-primary">Meer info</a>
+                </div>
             </div>
-        </div>
-        <div class="card">
-            <img class="card-img-top" src="images/miniature-figure.jpg" alt="Card image cap">
-            <div class="card-body">
-                <h5 class="card-title carousel-caption">Miniatuur figuur</h5>
-                <h6 class="card-subtitle mb-2 text-muted">[Prijs]</h6>
-                <h6 class="card-subtitle mb-2 text-muted">[Tijd]</h6>
-                <p class="card-text">De kraai is een in de symboliek vergelijkbaar met de raaf. Ook de kraai zou van
-                    oorsprong wit
-                    geweest zijn, in de Griekse sage wordt verteld dat de zonnegod Apollo zijn minnares, de
-                    koningsdochter Koronis, door een sneeuwwitte kraai....</p>
-                <a href="?url=detailpagina" class="btn btn-primary">Meer info</a>
+        </a>
+        <a href="?url=detailpagina">
+            <div class="card">
+                <img class="card-img-top" src="images/miniature-figure.jpg" alt="Card image cap">
+                <div class="card-body">
+                    <h5 class="card-title carousel-caption">Miniatuur figuur</h5>
+                    <h6 class="card-subtitle mb-2 text-muted">[Prijs]</h6>
+                    <h6 class="card-subtitle mb-2 text-muted">[Tijd]</h6>
+                    <p class="card-text">De kraai is een in de symboliek vergelijkbaar met de raaf. Ook de kraai zou van
+                        oorsprong wit
+                        geweest zijn, in de Griekse sage wordt verteld dat de zonnegod Apollo zijn minnares, de
+                        koningsdochter Koronis, door een sneeuwwitte kraai....
+                        <br>
+                        <br>
+                    </p>
+                    <a href="?url=detailpagina" class="btn btn-primary">Meer info</a>
+                </div>
             </div>
-        </div>
-        <div class="card">
-            <img class="card-img-top" src="images/Crow-Skull.png" alt="Card image cap">
-            <div class="card-body">
-                <h5 class="card-title carousel-caption">Kraaien schedel</h5>
-                <h6 class="card-subtitle mb-2 text-muted">[Prijs]</h6>
-                <h6 class="card-subtitle mb-2 text-muted">[Tijd]</h6>
-                <p class="card-text">De kraai is een in de symboliek vergelijkbaar met de raaf. Ook de kraai zou van
-                    oorsprong wit
-                    geweest zijn, in de Griekse sage wordt verteld dat de zonnegod Apollo zijn minnares, de
-                    koningsdochter Koronis, door een sneeuwwitte kraai....</p>
-                <a href="?url=detailpagina" class="btn btn-primary">Meer info</a>
+        </a>
+        <a href="?url=detailpagina">
+            <div class="card">
+                <img class="card-img-top" src="images/Crow-Skull.png" alt="Card image cap">
+                <div class="card-body">
+                    <h5 class="card-title carousel-caption">Kraaien schedel</h5>
+                    <h6 class="card-subtitle mb-2 text-muted">[Prijs]</h6>
+                    <h6 class="card-subtitle mb-2 text-muted">[Tijd]</h6>
+                    <p class="card-text">De kraai is een in de symboliek vergelijkbaar met de raaf. Ook de kraai zou van
+                        oorsprong wit
+                        geweest zijn, in de Griekse sage wordt verteld dat de zonnegod Apollo zijn minnares, de
+                        koningsdochter Koronis, door een sneeuwwitte kraai....
+                        <br>
+                        <br>
+                    </p>
+                    <a href="?url=detailpagina" class="btn btn-primary">Meer info</a>
+                </div>
             </div>
-        </div>
+        </a>
     </main>
 </div>
 
