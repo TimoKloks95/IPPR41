55c7d709d3e4fc83d998b3fec471ed088ef0af25
TIM-LAPTOP\tim-3 <tim-3@TIM-LAPTOP>
2018-05-24 14:39:17 +0200

    Responsive maken uitleg homepage

--- EenmaalAndermaal/home.php
+++ EenmaalAndermaal/home.php
@@ -10,11 +10,11 @@
 <main>
     <div class="container">
         <hr>
-        <h4 class="text-left text-inverse">Uitgelichte veilingen</h4>
+        <h4 class="text-left att text-inverse">Uitgelichte veilingen</h4>
         <hr>
         <div class="row">
             <a href="?url=detailpagina">
-                <div class="card col-3">
+                <div class="card col-12 col-sm-6 col-md-4 col-lg-3">
                     <img class="card-img-top" src="images/Crow-Skull.png" alt="Card image cap">
                     <div class="card-body">
                         <h5 class="card-title carousel-caption">Kraaien schedel</h5>
@@ -33,7 +33,7 @@
                 </div>
             </a>
             <a href="?url=detailpagina">
-                <div class="card col-3">
+                <div class="card col-12 col-sm-6 col-md-4 col-lg-3">
                     <img class="card-img-top" src="images/make-up.jpg" alt="Card image cap">
                     <div class="card-body">
                         <h5 class="card-title carousel-caption">Make-up</h5>
@@ -52,7 +52,7 @@
                 </div>
             </a>
             <a href="?url=detailpagina">
-                <div class="card col-3">
+                <div class="card col-12 col-sm-6 col-md-4 col-lg-3">
                     <img class="card-img-top" src="images/shoes.jpg" alt="Card image cap">
                     <div class="card-body">
                         <h5 class="card-title carousel-caption">Schoenen</h5>
@@ -71,7 +71,7 @@
                 </div>
             </a>
             <a href="?url=detailpagina">
-                <div class="card col-3">
+                <div class="card col-12 col-sm-6 col-md-4 col-lg-3">
                     <img class="card-img-top" src="images/wooden-closet.jpg" alt="Card image cap">
                     <div class="card-body">
                         <h5 class="card-title carousel-caption">Houten kast</h5>
@@ -91,11 +91,11 @@
             </a>
         </div>
         <hr>
-        <h4 class="text-left text-inverse">Nieuwste veilingen</h4>
+        <h4 class="text-left att text-inverse">Nieuwste veilingen</h4>
         <hr>
         <div class="row">
             <a href="?url=detailpagina">
-                <div class="card col-3">
+                <div class="card col-12 col-sm-6 col-md-4 col-lg-3">
                     <img class="card-img-top" src="images/Crow-Skull.png" alt="Card image cap">
                     <div class="card-body">
                         <h5 class="card-title carousel-caption">Kraaien schedel</h5>
@@ -114,7 +114,7 @@
                 </div>
             </a>
             <a href="?url=detailpagina">
-                <div class="card col-3">
+                <div class="card col-12 col-sm-6 col-md-4 col-lg-3">
                     <img class="card-img-top" src="images/make-up.jpg" alt="Card image cap">
                     <div class="card-body">
                         <h5 class="card-title carousel-caption">Make-up</h5>
@@ -133,7 +133,7 @@
                 </div>
             </a>
             <a href="?url=detailpagina">
-                <div class="card col-3">
+                <div class="card col-12 col-sm-6 col-md-4 col-lg-3">
                     <img class="card-img-top" src="images/shoes.jpg" alt="Card image cap">
                     <div class="card-body">
                         <h5 class="card-title carousel-caption">Schoenen</h5>
@@ -152,7 +152,7 @@
                 </div>
             </a>
             <a href="?url=detailpagina">
-                <div class="card col-3">
+                <div class="card col-12 col-sm-6 col-md-4 col-lg-3">
                     <img class="card-img-top" src="images/wooden-closet.jpg" alt="Card image cap">
                     <div class="card-body">
                         <h5 class="card-title carousel-caption">Houten kast</h5>
@@ -171,43 +171,48 @@
                 </div>
             </a>
         </div>
-    </div>
-</main>
-<div class="home-info">
-    <div class="jumbotron text-center">
-        <h1>Welkom bij EenmaalAndermaal!</h1>
-        <h3>Hoe werkt het en wat kun je ermee?</h3>
-        <br>
-        <br>
 
-        <div class="container">
-            <div class="row">
-                <div class="col-sm-3">
-                    <h3>Registreren/inloggen</h3>
-                    <p>Voordat u mee kunt bieden, moet u eerst inloggen met een account.</p><br>
-                    <p>Heeft u nog geen account? Dan kunt u zich hieronder registreren:</p>
-                </div>
-                <div class="col-sm-3">
-                    <h3>Veilingen zoeken</h3>
-                    <p>Bovenin kunt u kiezen om onze veilingen te bekijken. Ook kunt u deze per categorie tonen.</p>
-                    <p>Eveneens kunt u bovenin in de zoekbalk intypen wat voor veiling u zoekt. Deze zal alle veilingen
-                        tonen die overeen komen met uw zoekterm.</p>
-                </div>
-                <div class="col-sm-3">
-                    <h3>Bod plaatsen</h3>
-                    <p>Bij openstaande veilingen kunt u rechtsboven zien hoeveel tijd u nog heeft om mee te bieden.</p>
-                    <p>U vult het bedrag in dat u wenst te bieden en drukt op "Plaats bod". U kunt alleen meebieden wanneer
-                        uw bod hoger is dan het huidige hoogste bod.</p>
-                </div>
-                <div class="col-sm-3">
-                    <h3>Veiling aanmaken</h3>
-                    <p>DEZE TEKST ZAL WORDEN INGEVULD NADAT DEZE FUNCTIE IS UITGEWERKT</p>
-                    <p>DEZE TEKST ZAL WORDEN INGEVULD NADAT DEZE FUNCTIE IS UITGEWERKT</p>
-                </div>
-                <div class="container-homeregistreer col-sm-12">
-                    <a href="?url=registreer" type="button" class="btn btn-primary">Registreren</a>
+        <div class="home-info">
+            <div class="jumbotron text-center">
+                <h2>Welkom bij EenmaalAndermaal!</h2>
+                <h4>Hoe werkt het en wat kun je ermee?</h4>
+                <br>
+                <br>
+
+                <div class="container">
+                    <div class="row">
+                        <div class="col-sm-3">
+                            <h3>Inloggen</h3>
+                            <p>Voordat u mee kunt bieden, moet u eerst inloggen met een account.</p><br>
+                            <p>Heeft u nog geen account? Dan kunt u zich hieronder registreren:</p>
+                        </div>
+                        <div class="col-sm-3">
+                            <h3>Veilingen zoeken</h3>
+                            <p>Bovenin kunt u kiezen om onze veilingen te bekijken. Ook kunt u deze per categorie
+                                tonen.</p>
+                            <p>Eveneens kunt u bovenin in de zoekbalk intypen wat voor veiling u zoekt. Deze zal alle
+                                veilingen
+                                tonen die overeen komen met uw zoekterm.</p>
+                        </div>
+                        <div class="col-sm-3">
+                            <h3>Bod plaatsen</h3>
+                            <p>Bij openstaande veilingen kunt u rechtsboven zien hoeveel tijd u nog heeft om mee te
+                                bieden.</p>
+                            <p>U vult het bedrag in dat u wenst te bieden en drukt op "Plaats bod". U kunt alleen
+                                meebieden wanneer
+                                uw bod hoger is dan het huidige hoogste bod.</p>
+                        </div>
+                        <div class="col-sm-3">
+                            <h3>Veiling aanmaken</h3>
+                            <p>Voor het aanmaken van een veiling drukt u op "Veiling aanmaken".</p>
+                            <p>Zorg voor duidelijke foto's en een zinvolle beschrijving. Druk op "Veiling plaatsen" om de veiling te starten.</p>
+                        </div>
+                        <div class="container-homeregistreer col-sm-12">
+                            <a href="?url=registreer" type="button" class="btn btn-primary">Registreren</a>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
+</main>
